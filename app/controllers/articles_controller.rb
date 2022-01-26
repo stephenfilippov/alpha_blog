5d93fce38a4ba2c8451ead574445727d6a987b36
stephen <stephenfilippov@gmail.com>
2022-01-25 23:52:49 -0800

    created article controller with show and create

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+class ArticlesController < ApplicationController
+    def show 
+        @article = Article.find(params[:id])
+    end
+
+    def index 
+        @articles = Article.all
+    end
+
+    def new 
+        @article = Article.new
+    end
+
+    def create 
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+
+            redirect_to @article # the same as writing article_path(@article) = /articles/7
+        else
+            render "new" #displays the form again
+        end
+    end
+
+end
